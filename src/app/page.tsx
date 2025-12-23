2c5a73380fbbc0f3c061d80459b54a2b4792210f
Khenziii <khenzii.was.taken@gmail.com>
2025-12-23 20:56:06 +0100

    feat: add SASS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,14 @@
 import { type FC } from "react";
+import styles from "../styles/home.module.scss";
 
 const Home: FC = () => (
-    <div
-        style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            backgroundColor: "#676767",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection: "column",
-            gap: 10,
-        }}
-    >
+    <div className={styles["container"]}>
         <img
             src={"https://files.khenzii.dev/exploding-ryo.gif"}
             alt={"Ryo"}
             style={{ maxHeight: "300px" }}
         />
-        <p style={{ color: "lightblue" }}>{":3"}</p>
+        <p className={styles["text"]}>{":3"}</p>
     </div>
 );
 
