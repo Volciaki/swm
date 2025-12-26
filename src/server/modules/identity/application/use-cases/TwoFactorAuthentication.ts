9263b6f3308d5bb8df9a11461c2bdd0dcb9bbb24
Khenziii <khenzii.was.taken@gmail.com>
2025-12-26 19:53:25 +0100

    feat(api): create structure for user's management implementation

--- src/server/modules/identity/application/use-cases/TwoFactorAuthentication.ts
+++ src/server/modules/identity/application/use-cases/TwoFactorAuthentication.ts
@@ -3,11 +3,15 @@ import { AuthenticationTokenResponseDTO } from "../dto/LoginResponseDTO";
 import { TwoFactorAuthenticationDTO } from "../dto/TwoFactorAuthenticationDTO";
 import { AuthenticationManager } from "../services/AuthenticationManager";
 import { WrongTwoFactorAuthenticationValueError } from "../../domain/errors/WrongTwoFactorAuthenticationValue";
+import { AlreadyLoggedInError } from "../../domain/errors/AlreadyLoggedInError";
+import { User } from "../../domain/entities/User";
 
 export class TwoFactorAuthentication {
     constructor(private readonly authenticationManager: AuthenticationManager) {}
 
-    async execute(dto: TwoFactorAuthenticationDTO): Promise<AuthenticationTokenResponseDTO> {
+    async execute(dto: TwoFactorAuthenticationDTO, currentUser?: User): Promise<AuthenticationTokenResponseDTO> {
+        if (currentUser) throw new AlreadyLoggedInError();
+
         const authenticationID = UUID.fromString(dto.authenticationId);
         const authentication = this.authenticationManager.getTwoFactorAuthenticationSessionByID(authenticationID);
 
