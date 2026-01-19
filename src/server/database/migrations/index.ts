b37d7878130e0211fdd07ee62c73f5e7e1711229
Khenzii <khenzii@khenzii.dev>
2026-01-19 21:09:51 +0100

    chore(api): generate the database migration

--- src/server/database/migrations/index.ts
+++ src/server/database/migrations/index.ts
@@ -1,9 +1,11 @@
 import { UserManagement1767374352574 } from "./1767374352574-UserManagement";
 import { ProductManagement1767716678378 } from "./1767716678378-ProductManagement";
 import { FilesSupport1768605019891 } from "./1768605019891-FilesSupport";
+import { Notifications1768853259695 } from "./1768853259695-Notifications";
 
 export const migrations = [
 	UserManagement1767374352574,
 	ProductManagement1767716678378,
 	FilesSupport1768605019891,
+	Notifications1768853259695,
 ];
