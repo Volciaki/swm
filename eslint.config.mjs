27d2487c4fda6c94dac064c74e364df3929c3f56
Khenzii <khenzii@khenzii.dev>
2026-01-26 03:49:01 +0100

    feat: configure prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,32 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import importPlugin from "eslint-plugin-import";
+
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier";
+
+import importPlugin from "eslint-plugin-import";
+import prettierPlugin from "eslint-plugin-prettier";
 
 const eslintConfig = defineConfig([
 	...nextVitals,
 	...nextTs,
-	globalIgnores([
-		".next/**",
-		"dist/**",
-		"next-env.d.ts",
-	]),
+	globalIgnores([".next/**", "dist/**", "next-env.d.ts"]),
 	{
 		plugins: {
 			import: importPlugin,
+			prettier: prettierPlugin,
 		},
 		rules: {
-			quotes: ["error", "double", { "avoidEscape": true }],
-			indent: ["error", "tab", {
-				"ignoredNodes": ["PropertyDefinition[decorators]"]
-			}],
+			...prettierConfig.rules,
+			"prettier/prettier": "error",
+			quotes: ["error", "double", { avoidEscape: true }],
+			indent: [
+				"error",
+				"tab",
+				{
+					ignoredNodes: ["PropertyDefinition[decorators]"],
+				},
+			],
 			"import/no-duplicates": "error",
 			"object-curly-spacing": ["error", "always"],
 			"comma-spacing": ["error", { before: false, after: true }],
