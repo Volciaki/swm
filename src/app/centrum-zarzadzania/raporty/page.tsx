cc0f58247e9fc73966f12f30ec6ecf417571a6dc
Khenzii <khenzii@khenzii.dev>
2026-01-26 03:16:29 +0100

    feat(website): add placeholders of future pages

---
+++ src/app/centrum-zarzadzania/raporty/page.tsx
@@ -0,0 +1,11 @@
+import { type FC } from "react";
+import { Paragraph } from "@/ui/atoms";
+import { CenteredOnPage } from "@/ui/molecules";
+
+const Reports: FC = () => (
+	<CenteredOnPage>
+		<Paragraph>{"raporty"}</Paragraph>
+	</CenteredOnPage>
+);
+
+export default Reports;
