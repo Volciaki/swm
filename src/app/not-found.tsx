2b66c1f27891792a2f8779dfdfb05e13c48c7cb8
robigame1977 <dawigejm1977@gmail.com>
2026-02-07 18:21:07 +0100

    Ready 404 page, available for contributions

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,26 @@
 import { type FC } from "react";
-import { Paragraph } from "@/ui/atoms";
+import { Button, Flex, FullHeight, Paragraph } from "@/ui/atoms";
+import "./../styles/not-found.module.scss"
+import { PageHeader } from "@/ui/molecules";
 
-const NotFound: FC = () => <Paragraph>{"silly"}</Paragraph>;
+const NotFound: FC = () => {
+  return (
+    <FullHeight style={{ width: "100vw" }}>
+      <Flex direction={"column"} style={{ gap: "1rem", height: "70vh", width: "90%" }} fullWidth>
+        <Flex justify="flex-start" align="center" style={{ gap: "1rem", height: "auto", marginLeft: "3rem"}}>
+          <Paragraph fontSize={5} style={{textAlign: "center"}}>Error</Paragraph>
+          <Paragraph fontSize={10}>404</Paragraph>
+        </Flex>
+        <Flex justify="flex-end" style={{ gap: "1rem", height: "30%", marginRight: "5rem" }}>
+          <Paragraph fontSize={2.2} style={{textWrap:"wrap", width:"30%", textAlign:"center"}}>Ups... Najwyraźniej nie ma takiego regału w naszym magazynie</Paragraph>
+        </Flex>
+        <Flex justify="center" align="center" direction="column" style={{ gap: "2rem", height: "40%", marginTop: "4rem" }}>
+          <Paragraph variant="secondary" fontSize={1.3} style={{width:"20%", textAlign:"center"}}>Sprawdź adres URL, lub pozwól, że przekierujemy cię do strony głównej.</Paragraph>
+          <Button style={{width:"20%"}} variant="secondary"><Paragraph fontSize={1}>Przejdź do strony głównej</Paragraph></Button>
+        </Flex>
+      </Flex>
+    </FullHeight>
+  )
+};
 
 export default NotFound;
