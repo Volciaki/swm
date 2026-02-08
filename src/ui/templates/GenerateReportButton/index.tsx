4fde1c1ec5a91ffb6fc6acf14ba441d1bf9a2fd5
Khenzii <khenzii@khenzii.dev>
2026-02-08 18:04:24 +0100

    feat(website): make Reports page responsive

--- src/ui/templates/GenerateReportButton/index.tsx
+++ src/ui/templates/GenerateReportButton/index.tsx
@@ -3,6 +3,7 @@ import { Button, FormError, Loading, Paragraph } from "@/ui/atoms";
 import type { TRPCMutation } from "@/ui/types";
 import type { APIError } from "@/ui/utils";
 import { defaultErrorHandler } from "@/ui/utils";
+import { useMobile } from "@/ui/hooks";
 
 export type GenerateReportButtonProps<T extends TRPCMutation = TRPCMutation> = {
 	mutation: T;
@@ -17,6 +18,7 @@ export const GenerateReportButton = <T extends TRPCMutation = TRPCMutation>({
 }: GenerateReportButtonProps<T>) => {
 	const [error, setError] = useState<string | undefined>();
 	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const { mobile } = useMobile();
 
 	const onClickHandler = useCallback(async () => {
 		setIsLoading(true);
@@ -31,7 +33,7 @@ export const GenerateReportButton = <T extends TRPCMutation = TRPCMutation>({
 	return (
 		<>
 			<Button onClick={async () => await onClickHandler()}>
-				<Paragraph fontSize={1.5}>{text}</Paragraph>
+				<Paragraph fontSize={mobile ? 1.25 : 1.5}>{text}</Paragraph>
 			</Button>
 
 			{error && <FormError>{error}</FormError>}
