0d6ca1ced0ddf1b4198306c2701dae2f32dffb88
Khenzii <khenzii@khenzii.dev>
2026-02-08 19:28:51 +0100

    feat(website): make users management responsive

--- src/app/centrum-zarzadzania/uzytkownicy/nowy/page.tsx
+++ src/app/centrum-zarzadzania/uzytkownicy/nowy/page.tsx
@@ -1,21 +1,33 @@
+"use client";
+
 import { type FC } from "react";
 import { UserForm } from "@/ui/organisms";
 import { BackButton, PageHeader } from "@/ui/molecules";
 import { FullHeight, Flex } from "@/ui/atoms";
+import { useMobile } from "@/ui/hooks";
 
-const UsersNew: FC = () => (
-	<FullHeight>
-		<BackButton fallback={"/centrum-zarzadzania/uzytkownicy"} />
+const UsersNew: FC = () => {
+	const { mobile } = useMobile();
 
-		<Flex direction={"column"} align={"center"} style={{ gap: "1rem" }} fullWidth>
-			<PageHeader
-				title={"Dodaj użytkownika"}
-				description={"Używając poniższego formularza możesz dodać nowego użytkownika."}
-			/>
+	return (
+		<FullHeight>
+			<BackButton fallback={"/centrum-zarzadzania/uzytkownicy"} />
 
-			<UserForm />
-		</Flex>
-	</FullHeight>
-);
+			<Flex
+				direction={"column"}
+				align={"center"}
+				style={{ gap: "1rem", marginTop: mobile ? "1rem" : undefined }}
+				fullWidth
+			>
+				<PageHeader
+					title={"Dodaj użytkownika"}
+					description={"Używając poniższego formularza możesz dodać nowego użytkownika."}
+				/>
+
+				<UserForm />
+			</Flex>
+		</FullHeight>
+	);
+};
 
 export default UsersNew;
