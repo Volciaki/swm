2fc1e384d62d20a58ddd4af9ff31aec3b6fa48b0
Khenzii <khenzii@khenzii.dev>
2026-02-11 16:38:25 +0100

    fix(docs): update the Dockerfile

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 import { type FC } from "react";
 import { Paragraph } from "@/ui/atoms";
 
-const NotFound: FC = () => <Paragraph>{"silly"}</Paragraph>;
+const NotFound: FC = () => <Paragraph>{"Ta strona nie istnieje."}</Paragraph>;
 
 export default NotFound;
